7ebf7c27f380d8de95d20ac451f2b38bd0766632
陈廷硕 <chentingshuo@chentingshuo-MacBook-Pro.local>
2016-04-21 20:04:22 +0800

    Add MyAppList

---
+++ MyAppList/wear/src/main/java/com/example/chentingshuo/myapplist/Item.java
@@ -0,0 +1,13 @@
+package com.example.chentingshuo.myapplist;
+
+/**
+ * Created by chentingshuo on 16/4/21.
+ */
+public class Item {
+
+    public String title;
+
+    public Item(String title) {
+        this.title = title;
+    }
+}
---
+++ MyAppList/wear/src/main/java/com/example/chentingshuo/myapplist/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.chentingshuo.myapplist;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private Item item;
+    private RecyclerView recyclerView;
+    private AppListAdapter appListAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        item = new Item("hello world");
+
+        appListAdapter = new AppListAdapter(this, item);
+
+        recyclerView = (RecyclerView) findViewById(R.id.listview);
+        recyclerView.setAdapter(appListAdapter);
+
+
+
+
+    }
+    public class AppListAdapter extends RecyclerView.Adapter<ViewHolder> implements OnItemClickListener{
+
+
+
+        private final LayoutInflater inflater;
+
+        private Item item;
+
+        public AppListAdapter(Context context, Item item) {
+            this.inflater = LayoutInflater.from(context);
+            this.item = item;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(inflater.inflate(R.layout.list_item, parent, false), this);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+            holder.icon.setImageResource(R.mipmap.ic_launcher);
+            holder.title.setText(item.title);
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return super.getItemViewType(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return super.getItemId(position);
+        }
+
+        @Override
+        public void onItemClick(View view) {
+            Toast.makeText(MainActivity.this, "touch me", Toast.LENGTH_SHORT);
+        }
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public ImageView icon;
+        public TextView title;
+        public OnItemClickListener listener;
+
+        public ViewHolder(View itemview, OnItemClickListener listener) {
+            super(itemview);
+            this.icon = (ImageView) itemview.findViewById(R.id.icon);
+            this.title = (TextView) itemview.findViewById(R.id.title);
+            this.listener = listener;
+        }
+
+        @Override
+        public void onClick(View v) {
+            listener.onItemClick(v);
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view);
+    }
+}
