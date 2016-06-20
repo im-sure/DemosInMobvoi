d2280fff6933a80c7f440553030246ffa5458078
CloudChen <im_sure@163.com>
2016-06-20 11:37:20 +0800

    Add swipelayout

--- MyAppList/wear/src/main/java/com/example/chentingshuo/myapplist/MainActivity.java
+++ MyAppList/wear/src/main/java/com/example/chentingshuo/myapplist/MainActivity.java
@@ -10,13 +10,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends Activity {
 
     private final String TAG = "MainActivity";
     private Item[] item;
+    private ArrayList<Item> arrayList;
     private RecyclerView recyclerView;
     private AppListAdapter appListAdapter;
 
@@ -25,9 +30,12 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        item = new Item[]{new Item("one"), new Item("two")};
+        item = new Item[]{new Item("one"), new Item("two"), new Item("three"), new Item("four")
+                , new Item("five"), new Item("six"), new Item("seven"), new Item("eight")
+                , new Item("nine"), new Item("ten")};
+        arrayList = new ArrayList<Item>(Arrays.asList(item));
 
-        appListAdapter = new AppListAdapter(this, item);
+        appListAdapter = new AppListAdapter(this, arrayList);
 
         recyclerView = (RecyclerView) findViewById(R.id.listview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -37,37 +45,54 @@ public class MainActivity extends Activity {
 
 
     }
-    public class AppListAdapter extends RecyclerView.Adapter<ViewHolder> implements OnItemClickListener{
+    public class AppListAdapter extends RecyclerView.Adapter<ViewHolder> {
 
 
         Context context;
 
         private final LayoutInflater inflater;
 
-        private Item[] item;
+        private ArrayList<Item> arrayList;
 
-        public AppListAdapter(Context context, Item[] item) {
+        public AppListAdapter(Context context, ArrayList<Item> arrayList) {
             this.context = context;
             this.inflater = LayoutInflater.from(context);
-            this.item = item;
+            this.arrayList = arrayList;
         }
 
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new ViewHolder(inflater.inflate(R.layout.list_item, parent, false), this);
+            return new ViewHolder(inflater.inflate(R.layout.list_item, parent, false));
         }
 
         @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-
+        public void onBindViewHolder(final ViewHolder holder, final int position) {
+            holder.content.setX(0);
+            holder.swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);
             holder.icon.setImageResource(R.mipmap.ic_launcher);
-            holder.title.setText(item[position].title);
-            Log.d(TAG, "Current thread is " + Thread.currentThread().getId());
+            holder.title.setText(arrayList.get(position).title);
+            holder.content.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, holder.title.getText(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            holder.delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, holder.title.getText() + " deleted", Toast.LENGTH_SHORT).show();
+                    arrayList.remove(position);
+                    Log.d(TAG, "arrayList size is " + arrayList.size());
+                    notifyItemRemoved(position);
+                    notifyItemRangeChanged(position, arrayList.size());
+                }
+            });
         }
 
         @Override
         public int getItemCount() {
-            return item == null ? 0 : item.length;
+            return arrayList == null ? 0 : arrayList.size();
         }
 
         @Override
@@ -80,33 +105,26 @@ public class MainActivity extends Activity {
             return super.getItemId(position);
         }
 
-        @Override
-        public void onItemClick(View view) {
-            Toast.makeText(MainActivity.this, "touch me", Toast.LENGTH_SHORT).show();
-        }
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
-        public ImageView icon;
-        public TextView title;
-        public OnItemClickListener listener;
+        private SwipeLayout swipeLayout;
+        private ImageView icon;
+        private TextView title;
+        private RelativeLayout content;
+        private RelativeLayout delete;
 
-        public ViewHolder(View itemview, OnItemClickListener listener) {
+        public ViewHolder(View itemview) {
             super(itemview);
+            this.swipeLayout = (SwipeLayout) itemview.findViewById(R.id.swipe_layout);
             this.icon = (ImageView) itemview.findViewById(R.id.icon);
             this.title = (TextView) itemview.findViewById(R.id.title);
-            this.listener = listener;
-            itemview.setOnClickListener(this);
-        }
+            this.content = (RelativeLayout) itemview.findViewById(R.id.content);
+            this.delete = (RelativeLayout) itemview.findViewById(R.id.delete);
 
-        @Override
-        public void onClick(View v) {
-            listener.onItemClick(v);
+
         }
     }
 
-    public interface OnItemClickListener {
-        void onItemClick(View view);
-    }
 }
